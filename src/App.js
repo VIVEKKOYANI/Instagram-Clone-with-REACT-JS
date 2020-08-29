f1b9baad7d36694749f62736a5f381508c626765
vivek <vivek.koyani@gmail.com>
2020-08-29 12:18:08 +0530

    header create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import "./App.css";
 class App extends Component {
   render() {
     return (
       <>
-        <div>
+        <div className="app">
+          <div className="app__header">
+            <img
+              className="app__headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+            />
+          </div>
+
           <h1>Hello</h1>
+
+          {/* Header */}
+
+          {/* Posts */}
+
+          {/* Posts */}
         </div>
       </>
     );
